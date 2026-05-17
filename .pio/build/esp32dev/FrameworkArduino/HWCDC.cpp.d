b23059ae1273b2b649660f91e8d28711a9ceaf6e
MpM42 <Manuu22@live.com.ar>
2026-05-16 22:28:25 -0300

    Incorporando archivos WEB

--- .pio/build/esp32dev/FrameworkArduino/HWCDC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/HWCDC.cpp.o: \
- C:/Users/javit/.platformio/packages/framework-arduinoespressif32/cores/esp32/HWCDC.cpp \
- User_Setup.h \
- C:/Users/javit/.platformio/packages/framework-arduinoespressif32/cores/esp32/USB.h \
- C:/Users/javit/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
--- .pio/build/esp32dev/FrameworkArduino/libb64/cdecode.c.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/libb64/cdecode.c.o: \
- C:/Users/javit/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.c \
- User_Setup.h \
- C:/Users/javit/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cdecode.h
