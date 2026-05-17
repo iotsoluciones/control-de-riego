aa1e77251a91ca8d44e390ecfa717f21c559c5a5
Javier <Javito.her92.com>
2026-05-17 01:37:39 -0300

    agregando archivos

---
+++ .pio/build/esp32dev/FrameworkArduino/esp32-hal-tinyusb.c.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/esp32-hal-tinyusb.c.o: \
+ C:/Users/javit/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-tinyusb.c \
+ User_Setup.h \
+ C:/Users/javit/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
